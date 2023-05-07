a608348159ce1e521a4eb221f68f63984dc3660d
Ashlesh M D <113172856+ashlesh-md@users.noreply.github.com>
2023-05-07 20:05:18 +0530

    initial commit

---
+++ src/main/java/com/arjuncodes/springemaildemo/SpringEmailDemoApplication.java
@@ -0,0 +1,28 @@
+package com.arjuncodes.springemaildemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.mail.MessagingException;
+
+@SpringBootApplication
+public class SpringEmailDemoApplication {
+
+	@Autowired
+	private EmailSenderService senderService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailDemoApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() throws MessagingException {
+		senderService.sendSimpleEmail("emailsenrec@gmail.com",
+				"Testing email sender",
+				"If you get this mail...You are so lucky...!");
+
+	}
+}
